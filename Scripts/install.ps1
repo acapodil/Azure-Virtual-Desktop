d6caf9471510afd35bd9d09c6ac330924b5a28bc
acapodil <acapodilupo980@gmail.com>
2021-09-03 10:27:22 -0400

    added custom script for terraform config

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -29,7 +29,7 @@
 # To bypass the use of any proxy, please set $env:chocolateyIgnoreProxy = 'true'
 
 #use a different location to download the chocolatey nupkg file (anthony's github)
-$env:chocolateyDownloadUrl = 'https://github.com/apcapodilupo/WVD_2020/blob/main/Scripts/chocolatey.0.10.15.nupkg?raw=true'
+$env:chocolateyDownloadUrl = 'https://github.com/acapodil/Azure-Virtual-Desktop/blob/main/Scripts/chocolatey.0.10.15.nupkg?raw=true'
 			      
                                        
 
