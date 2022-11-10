9120e3aa63fb01099c2d9e273bb29e6c4540f5d4
acapodil <acapodilupo980@gmail.com>
2022-11-10 08:26:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ resource "azurerm_virtual_desktop_host_pool" "avdhppooled" {
 }
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "hp_token" {
+    count                      = var.vm_count
   hostpool_id     = azurerm_virtual_desktop_host_pool.avdhppooled.id
   expiration_date = timeadd(timestamp(), "2h")
 }
